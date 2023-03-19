623518532c056dc06d0f697a0d7f38b38b8873e4
kvolchok <kk.volchok@gmail.com>
2023-03-19 19:53:08 +0300

    feat: add sum of numbers

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main()
     {
-            
+        Console.WriteLine("Введите первое число:");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Введите второе число:");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+        
+        double sun = firstNumber + secondNumber;
+        Console.WriteLine("Результат сложения " + sun);
     }
 }
